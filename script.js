6aa533402b5abe87f959cd43d46d96e5e91fe6d9
PRITAM MANE <manepritam81@gmail.com>
2025-04-12 17:03:35 +0530

    certification

--- script.js
+++ script.js
@@ -2,4 +2,14 @@
 document.getElementById("themeToggle").addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
   });
+  function openModal(imgElement) {
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImage");
+    modal.style.display = "flex";
+    modalImg.src = imgElement.src;
+  }
+  
+  function closeModal() {
+    document.getElementById("imageModal").style.display = "none";
+  }
   
